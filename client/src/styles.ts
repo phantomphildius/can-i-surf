09f5774c4e10b36baa25404d88ad6736fec0bcb0
Andrew Freeman <andrew.freeman@betterment.com>
2021-12-18 22:39:42 -0500

    removes old style file

--- client/src/styles.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  flex: {
-    default: { display: 'flex' },
-    column: { display: 'flex', flexDirection: 'column' },
-  },
-  border: { border: '1px solid black' },
-} as const;
