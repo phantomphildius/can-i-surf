ebec01959c6c5a56a7a2474b7e1983e5831442bb
Andrew Freeman <andrew.freeman@betterment.com>
2021-12-29 09:24:53 -0500

    remove cruft

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById('root')
 );
